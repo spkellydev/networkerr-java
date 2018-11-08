7d86fbf6c5315a04d15a141838b428320ee65488
Sean Kelly <spkellydev@gmail.com>
2018-11-07 22:22:35 -0500

    Reflections library added, annotation mocked

--- src/main/java/com/networkerr/core/annotations/HttpEndpoint.java
+++ src/main/java/com/networkerr/core/annotations/HttpEndpoint.java
@@ -1,4 +1,14 @@
 package com.networkerr.core.annotations;
 
-public class HttpEndpoint {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface HttpEndpoint {
+    String route();
+    String method();
+    int statusCode();
 }
--- src/main/java/com/networkerr/core/routers/AnnotationRouter.java
+++ src/main/java/com/networkerr/core/routers/AnnotationRouter.java
@@ -1,4 +1,24 @@
 package com.networkerr.core.routers;
 
+import com.networkerr.core.annotations.HttpEndpoint;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+
+import java.lang.reflect.Method;
+import java.util.Set;
+
 public class AnnotationRouter {
+    public void findAnnotation() {
+        Reflections reflections = new Reflections(
+          new ConfigurationBuilder().setUrls(
+                  ClasspathHelper.forPackage("com.networkerr.app")
+          ).setScanners(
+                  new MethodAnnotationsScanner()
+          )
+        );
+        Set<Method> methods = reflections.getMethodsAnnotatedWith(HttpEndpoint.class);
+        System.out.println(methods);
+    }
 }
