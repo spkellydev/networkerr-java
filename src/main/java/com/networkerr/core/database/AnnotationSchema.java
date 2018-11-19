b457ab7ad28c609d040887d2c32cf29b1954d642
Sean Kelly <spkellydev@gmail.com>
2018-11-18 20:56:38 -0500

    Created Schema to find Annotations by references, set to stream

--- src/main/java/com/networkerr/core/database/AnnotationSchema.java
+++ src/main/java/com/networkerr/core/database/AnnotationSchema.java
@@ -5,8 +5,10 @@ import com.networkerr.core.annotations.SqlSchemaColumn;
 import com.networkerr.core.annotations.SqlSchemaTable;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Collection;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AnnotationSchema extends AnnotationsScanner {
     private Set types;
@@ -16,7 +18,7 @@ public class AnnotationSchema extends AnnotationsScanner {
     protected void getMapFromMethods() {
         this.findAnnotations("com.networkerr.app", SqlSchemaColumn.class);
         Collection<String> allModels = this.getAllOfClass(Model.class);
-        Annotation[] modelAnnotations = this.getAnnotationsFromClassCollection(allModels, SqlSchemaTable.class);
+        Annotation[][] modelAnnotations = this.getAnnotationsFromClassCollection(allModels, SqlSchemaTable.class);
         Annotation[] fieldAnnotations = this.getAnnotationsFromFields(allModels);
         String tableName = null;
         String references = null;
@@ -25,38 +27,64 @@ public class AnnotationSchema extends AnnotationsScanner {
         SQLTypes dataType = null;
         String[] flags = null;
         MySqlWriter writer = new MySqlWriter();
-        for(Annotation ann: modelAnnotations) {
-           if(ann.annotationType().equals(SqlSchemaTable.class)) {
-               try {
-                   tableName = (String) ann.annotationType().getMethod("table").invoke(ann);
-                   references = (String) ann.annotationType().getMethod("references").invoke(ann);
-                   foreignKey = (String) ann.annotationType().getMethod("foreignKey").invoke(ann);
-                   writer.createTableBegin(tableName);
-                   System.out.println(tableName);
-               } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-                   e.printStackTrace();
-               }
-           }
-        }
-
-        for(int i = 0; i < fieldAnnotations.length; i++) {
-            Annotation ann = fieldAnnotations[i];
-            boolean isLast = i == fieldAnnotations.length - 1;
-            if(ann.annotationType().equals(SqlSchemaColumn.class)) {
-                try {
-                    columnName = (String) ann.annotationType().getMethod("column").invoke(ann);
-                    dataType = (SQLTypes) ann.annotationType().getMethod("dataType").invoke(ann);
-                    flags = (String[]) ann.annotationType().getMethod("properties").invoke(ann);
-                    writer.createTableColumn(columnName, isLast, dataType, flags);
-                    writer.createTableEnd();
-                    String sql = writer.getStatement();
-                    writer.addToTables(sql);
-                } catch (Exception e) {
-                    e.printStackTrace();
+        // create hashmap for all of the available tables
+        Collection<Schema> tables = new ArrayList<>();
+        for(Annotation[] annotate: modelAnnotations) {
+            for(Annotation ann: annotate) {
+                if (ann.annotationType().equals(SqlSchemaTable.class)) {
+                    try {
+                        tableName = (String) ann.annotationType().getMethod("table").invoke(ann);
+                        references = (String) ann.annotationType().getMethod("references").invoke(ann);
+                        foreignKey = (String) ann.annotationType().getMethod("foreignKey").invoke(ann);
+                        // add to tables Collection
+                        Schema t = new Schema(tableName, references, foreignKey);
+                        tables.add(t);
+                    } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
-        this.setSeed(writer.getTableQuery());
+
+        // create final string for lambda reference check
+        final String finalReferences = references;
+        // filter current collection state to see if a table name equals current "references" annotation
+        Stream<Schema> referenced = tables.stream()
+                .filter((Schema table) -> table
+                        .getTableName()
+                        .equals(finalReferences)
+                );
+        // create immutable list of existing references
+        List<Schema> existingRef = Collections.unmodifiableList(
+                // filter out nulls and collect referenced annotations
+                referenced.filter(Objects::nonNull).collect(Collectors.toList())
+        );
+        if (!existingRef.isEmpty()) {
+            existingRef.forEach((Schema referencedSchema) -> {
+//                System.out.println(referencedSchema.getTableName());
+                
+            });
+        }
+
+//
+//        for(int i = 0; i < fieldAnnotations.length; i++) {
+//            Annotation ann = fieldAnnotations[i];
+//            boolean isLast = i == fieldAnnotations.length - 1;
+//            if(ann.annotationType().equals(SqlSchemaColumn.class)) {
+//                try {
+//                    columnName = (String) ann.annotationType().getMethod("column").invoke(ann);
+//                    dataType = (SQLTypes) ann.annotationType().getMethod("dataType").invoke(ann);
+//                    flags = (String[]) ann.annotationType().getMethod("properties").invoke(ann);
+//                    writer.createTableColumn(columnName, isLast, dataType, flags);
+//                    writer.createTableEnd();
+//                    String sql = writer.getStatement();
+//                    writer.addToTables(sql);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        this.setSeed(writer.getTableQuery());
     }
 
     protected String getSeed() {
--- src/main/java/com/networkerr/core/database/Schema.java
+++ src/main/java/com/networkerr/core/database/Schema.java
@@ -1,4 +1,21 @@
 package com.networkerr.core.database;
 
 public class Schema {
+    String tableName;
+    String references;
+    String foreignKey;
+
+    public Schema(String tableName, String references, String foreignKey) {
+        this.tableName = tableName;
+        this.references = references;
+        this.foreignKey = foreignKey;
+    }
+
+    public String toString() {
+        return tableName + " " + references + " " + foreignKey;
+    }
+
+    public String getTableName() {
+        return this.tableName;
+    }
 }
